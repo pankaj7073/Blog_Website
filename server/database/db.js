5e3272ce7c40f0e3400c18b3cfbad6d003c4f3e8
pankaj7073 <pankajvaishnav128@gmail.com>
2023-07-19 12:11:51 +0530

    changes json files

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const Connection = async () => {
-  const URL = `mongodb+srv://pv516712:pankaj9468@cluster0.ifqocod.mongodb.net/Blog_Users?retryWrites=true&w=majority`;
+  const URL = process.env.MONGO_URL;
   try {
     await mongoose.connect(URL, { useNewUrlParser: true });
     console.log("Database connected successfully");
