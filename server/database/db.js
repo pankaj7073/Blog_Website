3dce55898ee75f3479f4a4e4544c517c929f96c8
Pankaj Swami Vaishnav <104480147+pankaj7073@users.noreply.github.com>
2023-07-19 14:36:29 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 const Connection = async () => {
   const URL = process.env.MONGO_URL;
   try {
-    await mongoose.connect(URL, { useNewUrlParser: true });
+    await mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true, });
     console.log("Database connected successfully");
   } catch (error) {
     console.log("Error while connecting to the database ", error);
